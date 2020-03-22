2cb0516bd12f70c5663532ff1bfb59e602065a57
Jonathan Herrmann <jonathan.herrmann@tum.de>
2020-03-22 13:58:06 +0100

    update standalone script and README.md
    Added new datatype flag. Script now supports retrieval from csv data. Additionally the code was polished a bit

--- standalone-script/README.md
+++ standalone-script/README.md
@@ -2,3 +2,4 @@ This script can handle 3 commandline parameters:
 - '-apikey': specify your API key. If not provided it defaults to 'demo'
 - '-symbol': specify the symbol of the stock you want to get the data from. If not provided it defaults to 'MSFT'
 - '-function': specify which function you would like to call. At the moment 'intraday' and 'quote' are supported. If not provided it defaults to 'intraday'
+- '-datatype': specify in which form the data should be retrieved as. Valid options are 'json' and 'csv'. If not provided it defaults to 'json'
--- standalone-script/alphavantage.go
+++ standalone-script/alphavantage.go
@@ -1,23 +1,44 @@
-package alphavantage
+package main
 
 import (
 	"bytes"
 	"encoding/csv"
 	"encoding/json"
+	"errors"
+	"flag"
 	"fmt"
 	"io"
+	"log"
+	"net/http"
 	"os"
 	"sort"
 	"strconv"
 	"strings"
+	"time"
 )
 
-// readIntradayData reads converts the JSON object to a map and reads the necessary data
-func readIntradayData(httpResponseBody io.Reader, interval *int64) [][]string {
+// readIntradayCSV expects a csv file in the response body. It returns it as a 2D array
+func readIntradayCSV(response *http.Response) ([][]string, error) {
+	reader := csv.NewReader(response.Body)
+	var results [][]string
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return nil, err
+		}
+		results = append(results, record)
+	}
+	return results, nil
+}
+
+// readIntradayData expects a JSON object in the response body. It returns it as a 2D array
+func readIntradayDataJSON(response *http.Response, interval *int64) ([][]string, error) {
 	// Read response as JSON
 	// create map interface
 	var i map[string]map[string]map[string]interface{}
-	json.NewDecoder(httpResponseBody).Decode(&i)
+	json.NewDecoder(response.Body).Decode(&i)
 
 	// create table to store the values in
 	recordList := make([]string, 0, 105)
@@ -36,32 +57,28 @@ func readIntradayData(httpResponseBody io.Reader, interval *int64) [][]string {
 			buffer.WriteString(",")
 			buffer.WriteString(str)
 		} else {
-			fmt.Println("Error: 'open' not a string")
-			return nil
+			return nil, errors.New("Error: 'open' not a string")
 		}
 		// get high
 		if str, ok := v["2. high"].(string); ok {
 			buffer.WriteString(",")
 			buffer.WriteString(str)
 		} else {
-			fmt.Println("Error: 'high' not a string")
-			return nil
+			return nil, errors.New("Error: 'high' not a string")
 		}
 		// geht low
 		if str, ok := v["3. low"].(string); ok {
 			buffer.WriteString(",")
 			buffer.WriteString(str)
 		} else {
-			fmt.Println("Error: 'low' not a string")
-			return nil
+			return nil, errors.New("Error: 'low' not a string")
 		}
 		// get volume
 		if str, ok := v["5. volume"].(string); ok {
 			buffer.WriteString(",")
 			buffer.WriteString(str)
 		} else {
-			fmt.Println("Error: 'volume' not a string")
-			return nil
+			return nil, errors.New("Error: 'volume' not a string")
 		}
 
 		recordList = append(recordList, buffer.String())
@@ -88,15 +105,32 @@ func readIntradayData(httpResponseBody io.Reader, interval *int64) [][]string {
 		row := strings.Split(item, ",")
 		table = append(table, row)
 	}
-	return table
+
+	return table, nil
 }
 
-// readGlobalQuoteData converts the JSON object to a map and reads the data
-func readGlobalQuoteData(httpResponseBody io.Reader) [][]string {
+// readGlobalQuoteCSV expects a csv file in the response body. It returns it as a 2D array
+func readGlobalQuoteCSV(response *http.Response) ([][]string, error) {
+	reader := csv.NewReader(response.Body)
+	var results [][]string
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return nil, err
+		}
+		results = append(results, record)
+	}
+	return results, nil
+}
+
+// readGlobalQuoteData expects a JSON object in the response body. It returns it as a 2D array
+func readGlobalQuoteDataJSON(response *http.Response) ([][]string, error) {
 	// Read response as JSON
 	// create map interface
 	var i map[string]map[string]interface{}
-	json.NewDecoder(httpResponseBody).Decode(&i)
+	json.NewDecoder(response.Body).Decode(&i)
 
 	table := make([][]string, 0, 2)
 
@@ -123,88 +157,78 @@ func readGlobalQuoteData(httpResponseBody io.Reader) [][]string {
 	if str, ok := i["Global Quote"]["01. symbol"].(string); ok {
 		dataRow[0] = str
 	} else {
-		fmt.Println("Error: 'symbol' not a string")
-		return nil
+		return nil, errors.New("Error: 'symbol' not a string")
 	}
 	// get open
 	if str, ok := i["Global Quote"]["02. open"].(string); ok {
 		dataRow[1] = str
 	} else {
-		fmt.Println("Error: 'open' not a string")
-		return nil
+		return nil, errors.New("Error: 'open' not a string")
 	}
 	// get high
 	if str, ok := i["Global Quote"]["03. high"].(string); ok {
 		dataRow[2] = str
 	} else {
-		fmt.Println("Error: 'high' not a string")
-		return nil
+		return nil, errors.New("Error: 'high' not a string")
 	}
 	// get low
 	if str, ok := i["Global Quote"]["04. low"].(string); ok {
 		dataRow[3] = str
 	} else {
-		fmt.Println("Error: 'low' not a string")
-		return nil
+		return nil, errors.New("Error: 'low' not a string")
 	}
 	// get price
 	if str, ok := i["Global Quote"]["05. price"].(string); ok {
 		dataRow[4] = str
 	} else {
-		fmt.Println("Error: 'price' not a string")
-		return nil
+		return nil, errors.New("Error: 'price' not a string")
 	}
 	// get volume
 	if str, ok := i["Global Quote"]["06. volume"].(string); ok {
 		dataRow[5] = str
 	} else {
-		fmt.Println("Error: 'volume' not a string")
-		return nil
+		return nil, errors.New("Error: 'volume' not a string")
 	}
 	// get latest trading day
 	if str, ok := i["Global Quote"]["07. latest trading day"].(string); ok {
 		dataRow[6] = str
 	} else {
-		fmt.Println("Error: 'latest trading day' not a string")
-		return nil
+		return nil, errors.New("Error: 'latest trading day' not a string")
 	}
 	// get previous close
 	if str, ok := i["Global Quote"]["08. previous close"].(string); ok {
 		dataRow[7] = str
 	} else {
-		fmt.Println("Error: 'previous close' not a string")
-		return nil
+		return nil, errors.New("Error: 'previous close' not a string")
 	}
 	// get change
 	if str, ok := i["Global Quote"]["09. change"].(string); ok {
 		dataRow[8] = str
 	} else {
-		fmt.Println("Error: 'chagne' not a string")
-		return nil
+		return nil, errors.New("Error: 'change' not a string")
 	}
 	// get change percent
 	if str, ok := i["Global Quote"]["10. change percent"].(string); ok {
 		dataRow[9] = str
 	} else {
-		fmt.Println("Error: 'change percent' not a string")
-		return nil
+		return nil, errors.New("Error: 'change percent' not a string")
 	}
 
 	// append to table
 	table = append(table, dataRow)
 
-	return table
+	return table, nil
 }
 
 // WriteToCSV takes a 2D Array and a pointer to the function and writes the data to a csv file
-func WriteToCSV(table *[][]string, function *string) {
+func WriteToCSV(table *[][]string, function *string) error {
 	// write to csv file
 	// create new file
 	fmt.Println("Writing CSV File")
 	// truncate existing files, create non-existing files, open in write only mode
 	file, err := os.OpenFile(*function+".csv", os.O_TRUNC|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		panic(err)
+		return error
 	}
 	defer file.Close()
 
@@ -214,6 +238,127 @@ func WriteToCSV(table *[][]string, function *string) {
 	// write file
 	writerFailiure := csvWriter.WriteAll(*table)
 	if writerFailiure != nil {
-		fmt.Println(writerFailiure)
+		return writerFailiure
 	}
+
+	return nil
+}
+
+func main() {
+	// parse cmd line flags
+	var apiKey = flag.String("apikey", "demo", "Set the API Key to connet to Alpha Vantage")
+	var symbol = flag.String("symbol", "MSFT", "Define the stock you want to get the data from")
+	var interval = flag.Int64("interval", 5, "Set the interval between data points. Valid are 1, 5, 15, 30 and 60")
+	var function = flag.String("function", "intraday", "Specify what data you want to retrieve. Valid values are 'intraday' and 'quote'")
+	var datatype = flag.String("datatype", "json", "Specify if the data should be retrieved as JSON or CSV")
+
+	flag.Parse()
+
+	// check for valid parameters
+	if *interval != 1 && *interval != 5 && *interval != 15 && *interval != 30 && *interval != 60 {
+		fmt.Println("Invalid interval provided. Provided value:", *interval, "\nValid Values: 1, 5, 15, 30, 60")
+		return
+	}
+
+	if *function == "intraday" {
+		*function = "TIME_SERIES_INTRADAY"
+	} else if *function == "quote" {
+		*function = "GLOBAL_QUOTE"
+	} else {
+		fmt.Println("Invalid function call. Provided value:", *function, "\nValid Values: 'intraday' or 'quote'")
+		return
+	}
+
+	if *datatype != "json" && *datatype != "csv" {
+		fmt.Println("Invalid datatype. Provided value:", *datatype, "\nValid Values: 'json' or 'csv'")
+	}
+
+	// print flags
+	fmt.Println("--- Flags ---")
+	fmt.Println("Symbol:", *symbol)
+	fmt.Println("API Key:", *apiKey)
+	fmt.Println("Interval:", *interval)
+	fmt.Println("Function:", *function)
+	fmt.Println("Datatype:", *datatype)
+	fmt.Println("-------------\n")
+
+	// create new client
+	client := &http.Client{
+		Timeout: 10 * time.Second,
+	}
+
+	// full url: https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&apikey=demo
+
+	urlBase := "https://www.alphavantage.co/query?"
+
+	// append function
+	urlBase += "function=" + *function
+	// append symbol
+	urlBase += "&symbol=" + *symbol
+	// append interval
+	urlBase += "&interval=" + strconv.FormatInt(*interval, 10) + "min"
+	// append api key
+	urlBase += "&apikey=" + *apiKey
+
+	// if datatype is csv append to url
+	if *datatype == "csv" {
+		urlBase += "&datatype=csv"
+	}
+
+	fmt.Println("Sending Request")
+
+	// send HTTP GET request
+	httpResponse, err := client.Get(urlBase)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer httpResponse.Body.Close()
+
+	fmt.Println("Decoding Response")
+
+	// depending on the requested function a different handler has to be called
+	var table [][]string
+	switch *function {
+	case "TIME_SERIES_INTRADAY":
+		if *datatype == "csv" {
+			table, parseError := readIntradayCSV(httpResponse)
+			if table == nil && parseError != nil {
+				log.Fatal(parseError)
+			}
+		} else {
+			table, parseError := readIntradayDataJSON(httpResponse, interval)
+			if table == nil && parseError != nil {
+				log.Fatal(parseError)
+			}
+		}
+
+		break
+
+	case "GLOBAL_QUOTE":
+		if *datatype == "csv" {
+			table, parseError := readGlobalQuoteCSV(httpResponse)
+			if table == nil && parseError != nil {
+				log.Fatal(parseError)
+			}
+		} else {
+			table, parseError := readGlobalQuoteDataJSON(httpResponse)
+			if table == nil && parseError != nil {
+				log.Fatal(parseError)
+			}
+		}
+
+		break
+
+	default:
+		fmt.Println("Unknown function call")
+		return
+	}
+
+	writerError := WriteToCSV(&table, function)
+	if writerError != nil {
+		log.Fatal(writerError)
+	}
+
+	fmt.Println("Done")
+
 }
